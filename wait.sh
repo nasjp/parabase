17fc5be60a6ade12cf360e2a88196b2449ddfa8e
nas <nas.silver.bullet@gmail.com>
2021-06-09 19:20:30 +0900

    fix wait

--- wait.sh
+++ wait.sh
@@ -3,4 +3,5 @@
 until mysqladmin ping -h$MYSQL_HOST -P$MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASSWORD;
 do
   sleep 1
+  echo "mysqladmin ping -h$MYSQL_HOST -P$MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASSWORD;"
 done
